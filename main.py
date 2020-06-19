170acde6fe93b7625b29954cc206feb76ed7dcf1
nikolin <niko.majmari@gmail.com>
2020-06-19 13:43:39 +0000

    Initialized main

--- main.py
+++ main.py
@@ -1,1 +1,61 @@
+from database import Database
+from bills import *
+from identity import *
+from reports import * 
+
+
+
+
+
+
+
+
+
+def main():
+  # loading 
+  db = Database(dbname="database")
+  # create tables here 
+  #
+  while(True):
+    # check if there is any account 
+    # if it is ask for log in 
+    # login 
+    # register 
+    # 
+    # if login sucesfull 
+    while(True):
+      # menuja kryesore 
+      #if else  if else 
+      choice = input(">")
+      if(choice==1):
+        while(False):
+          pass
+        pass
+      else:
+        pass
+
+
+
+
+
+
+
+
+
+
+    
+
+
+
+
+
+
+if __name__ == '__main__':
+    main()
+
+
+
+
+
+
 
