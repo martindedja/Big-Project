1a19626a18b447aa70ed89f5ecc3c7d1593e8a4b
Stefano Mihallari <stefanomihallari@gmail.com>
2020-06-19 14:16:17 +0000

    stefo

--- main.py
+++ main.py
@@ -12,7 +12,29 @@ def main():
   db.createtableifnotexists("Accounts",Account,Account.fromstring)
   while(True):
     # check if there is any account 
-    # if it is ask for log in 
+    all_accounts=db.getObjectsFrom("Accounts",condition= lambda a:True)
+    if len(all_accounts) ==0:
+      print("Register the account.")
+      #TODO add new account
+    else:
+      print("Press 1 for login, 2 to register and 3 for exiting.")
+      a=int(input())
+       
+      if a==1:
+        pass
+        #TODO login
+      elif a==2:
+        pass
+        #TODO crete new Account
+      elif a==3:
+        pass
+        #TODO exit
+      else:
+        continue
+
+
+    # if it is ask for log in
+
     # login 
     # register 
     # 
