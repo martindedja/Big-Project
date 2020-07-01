4b4004b89f1ef5d62e9fc966caf416e91af88b77
unknown <martindedja.martindedja11@gmail.com>
2020-07-01 02:26:11 +0200

    new new

--- bills.py
+++ bills.py
@@ -1,10 +1,7 @@
 from database import Database
 from prettytable import PrettyTable
-
-
-
+from colorama import Fore
 db = Database(dbname="database")
-
 class AllBills:
   def __init__(self,ID,total,date,month,year,status,type_,product_name,username):
     self.ID= ID
@@ -31,11 +28,17 @@ class AllBills:
       if cl1[5] == 'not_paid' or 'paid':
         cl2 = AllBills(cl1[0],cl1[1],cl1[2],cl1[3],cl1[4],cl1[5],cl1[6],cl1[7],user)
         db.appendObjectInto("All Bills",cl2)
+        print(Fore.GREEN)
+        print("Bill added")
+        print(Fore.RESET)
       else:
+        print(Fore.RED)
         print("Bill status error! write not_paid or paid!!")
+        print(Fore.RESET)
     else:
+      print(Fore.RED)
       print("Wrong bill structure")
-
+      print(Fore.RESET)
 #------------------Getters------------------#
   def getID(self):
     return self.ID 
@@ -74,42 +77,47 @@ class AllBills:
     self.product_name = product_name
   def setusername(self, username):
     self.username = username
-
 #21/2134/4/2020/paid/FaturaMujore/MathiasD
 #21/2134/4/2020/paid/RandomBill/MathiasD
 #21/2134/4/2020/paid/GasBill/MathiasD/Nafte
 #21/2134/4/2020/paid/ProductBill/MathiasD/Patate
-
 class ManageBills():
   @staticmethod
-  def createBill(db, user):
+  def createBill(db,user):
     #dict = {1:FaturaMujore, 2:RandomBill}
-    print("Enter the bill's ID, total, day,month, year, status,type,product name|| all seperated by /: ")
+    print("Enter the bill's ID, total, day,month, year, status,type,product name,your username|| all seperated by /: ")
     keep_going=True
     while keep_going==True:
-      clas_input = str(input())
-      cls1 = clas_input.split("/")
-      if len(cls1) == 8 and cls1[1].isdigit() and cls1[2].isdigit() and cls1[3].isdigit() and cls1[4].isdigit():
-        if cls1[5] == 'not_paid' or cls1[5] == 'paid':
-          if int(cls1[2])>31 or int(cls1[2])<1:
-            print("Check the date.")
-            continue
-          elif int(cls1[3])<1 or int(cls1[3])>12:
-            print("Check the month.")
-            continue
-          elif int(cls1[4])<2000:
-            print("Check the year:")
-            continue
-          else:
-            cls2 = AllBills(cls1[0],cls1[1],cls1[2],cls1[3], cls1[4],cls1[5],cls1[6],cls1[7],user)
-            db.appendObjectInto("All Bills",cls2)
-            print("Bill added!")
-            keep_going=False
+      try:
+        clas_input = str(input())
+        cls1 = clas_input.split("/")
+        if int(cls1[2])>31 or int(cls1[2])<1:
+          print(Fore.RED)
+          print("Check the date.")
+          print(Fore.RESET)
+          continue
+        elif int(cls1[3])<1 or int(cls1[3])>12:
+          print(Fore.RED)
+          print("Check the month.")
+          print(Fore.RESET)
+          continue
+        elif int(cls1[4])<2000:
+          print(Fore.RED)
+          print("Check the year.")
+          print(Fore.RESET)
+          continue
         else:
-          print("Check the status")
-      else:
-        print("Incorrect date or bill structure")
-
+          cls2 = AllBills(cls1[0],cls1[1],cls1[2],cls1[3], cls1[4],cls1[5],cls1[6],cls1[7],user)  
+          db.appendObjectInto("All Bills",cls2)
+          print(Fore.GREEN)
+          print("Bill added!")
+          print(Fore.RESET)
+          keep_going=False          
+      except Exception:
+        print(Fore.RED)
+        print("Error")
+        print(Fore.RESET)
+        pass
   @staticmethod
   def check_smaller_date(list1,min_day,min_month,min_year):
     returned_list = [ ]
@@ -130,8 +138,6 @@ class ManageBills():
       else:
         returned_list.append(bill)
     return returned_list
-  
-
   @staticmethod
   def check_limit_date(list1,max_day,max_month,max_year):
     returned_list = [ ]
@@ -151,101 +157,71 @@ class ManageBills():
         pass
       else:
         returned_list.append(bill)
-
     return returned_list
-
-
   @staticmethod
   def filterBill(db,username):
-    print("Press 1 to filter by Type: ")
+    print("\nPress 1 to filter by Type: ")
     print("Press 2 to filter by Deadline: ")
     print("Press 3 to filter by Total: ")
-
-    user_input= input()
-    if user_input.isdigit():
-      user_input = int(user_input)
-    else:
-      print('Wrong input')
+    print("Press 4 to go back to main menu: ")
+    user_input= int(input(">\n"))
     if user_input == 1:
       type_input = input("Give the type of bill you'd like to search: ")
       if len(db.getObjectsFrom("All Bills", lambda x: x.username == username))!=0:
         filtered_list = db.getObjectsFrom("All Bills", lambda x: x.type_ == type_input and x.username == username)
         if len(filtered_list) != 0:
           ManageBills.show_my_filtered_bills(db, filtered_list)
-      
-      
-    
     elif user_input == 2:
       min_deadline = input("Start date dd/mm/yyyy format: ").split("/")
-      if len(min_deadline) == 3 and min_deadline[0].isdigit() and min_deadline[1].isdigit() and min_deadline[2].isdigit():
-        max_deadline = input("End date  dd/mm/yyyy format: ").split("/")
-        if len(max_deadline) == 3 and max_deadline[0].isdigit() and max_deadline[1].isdigit() and max_deadline[2].isdigit():
-          min_day,min_month,min_year = int(min_deadline[0]), int(min_deadline[1]), int(min_deadline[2])
-          max_day,max_month,max_year = int(max_deadline[0]), int(max_deadline[1]), int(max_deadline[2])
-          list1 = db.getObjectsFrom("All Bills", lambda x:True)
-          min_date_checked_bills = ManageBills.check_smaller_date(list1, min_day,min_month,min_year)
-          in_deadline_bills = ManageBills.check_limit_date(min_date_checked_bills, max_day, max_month, max_year)
-          for bill in in_deadline_bills:
-            print(bill.date, bill.month, bill.year,"\n")
-        else:
-          print("Incorrect end date!")
-      else:
-        print("Incorrect start date!")
+      max_deadline = input("End date  dd/mm/yyyy format: ").split("/")
+      min_day,min_month,min_year = int(min_deadline[0]), int(min_deadline[1]), int(min_deadline[2])
+      max_day,max_month,max_year = int(max_deadline[0]), int(max_deadline[1]), int(max_deadline[2])
+      list1 = db.getObjectsFrom("All Bills", lambda x:True)
+      min_date_checked_bills = ManageBills.check_smaller_date(list1, min_day,min_month,min_year)
+      in_deadline_bills = ManageBills.check_limit_date(min_date_checked_bills, max_day, max_month, max_year)
+      ManageBills.show_my_filtered_bills(db, in_deadline_bills)
     elif user_input == 3:
       min_total = int(input("Enter min total: "))
-      max_total = int(input("Enter max total: "))
-      if min_total.isdigit() and max_total.isdigit():
-        if len(db.getObjectsFrom("All Bills", lambda x: x.username == username))!=0:
-          a = db.getObjectsFrom("All Bills", lambda x: min_total<= int(x.total) <= max_total and x.username == username )
-          if len(a)!=0:
-            ManageBills.show_my_filtered_bills(db, a)
+      max_total = int(input("Enter max total: ")) 
+      if len(db.getObjectsFrom("All Bills", lambda x: x.username == username))!=0:
+        a = db.getObjectsFrom("All Bills", lambda x: min_total<= int(x.total) <= max_total and x.username == username )
+        if len(a)!=0:
+          ManageBills.show_my_filtered_bills(db, a)
     else:
-    	print("\n")
-    	return
-
+      pass
   @staticmethod
   def deleteBill(db,username):
     print("Enter the ID and Type of the bill you want to delete : ")
     ID = input("ID = ")
     Type = input("Type = ")
     db.deleteObjectsFrom("All Bills",lambda a: a.ID == ID and a.username==username and a.type_ == Type)
-    print("Bill succssesfully deleted!")
-  @staticmethod
-  def ModifyBill(db):
-    print("Press 1 to modify gas bills\nPress 2 to modify monthly bill\n Press 3 to modify Product bill \n press 4 to modify a random bill")
-    choice=input(">")
-    if choice=='1':
-      print("Press 1 to change ID \nPress 2 to modify \n Press 3 to modify Product bill \n press 4 to modify a random bill")
-      pass
-    pass
-
+    print(Fore.GREEN)
+    print("Bill successfully deleted!")
+    print(Fore.RESET)
   @staticmethod
   def unpaid_bills(db,username):
     a = (db.getObjectsFrom("All Bills", lambda x : x.username == username and x.status=="not_paid"))
     if len(a)!=0:
       ManageBills.show_my_filtered_bills(db, a)
       print("Press 1 to select a bill and declare it as paid or press 2 to go to the main menu.")
-      choice=input(">")
-      if choice.isdigit():
-        choice = int(choice)
-
+      choice=int(input(">"))
       if choice==2:
         pass
       elif choice==1:
         thatid=str(input("Please enter the bill's ID:"))
         ap=db.getObjectsFrom("All Bills", lambda x : x.username == username and x.ID==thatid)
         ap[0].setstatus("paid")
-       
         w=db.getObjectsFrom("All Bills",lambda x:x.username==username and x.ID!=thatid)
         w.append(ap[0])
         db.overwriteObjectsInto("All Bills", w)
-        print("Bill declared as paid.")       
+        print(Fore.YELLOW)
+        print("Bill declared as paid.")     
+        print(Fore.RESET)  
     else:
+      print(Fore.ORANGE)
       print("You do not have any unpaid bills")
+      print(Fore.RESET)
       pass
-  #2861/2134/28/07/2019/paid/GasBill/Nafte/MathiasD
-  
-  
   @staticmethod
   def showmybills(db,username):
     if len(db.getObjectsFrom("All Bills", lambda x : x.username == username))!=0:
@@ -254,11 +230,10 @@ class ManageBills():
       for bill in a:
         t.add_row([bill.ID, bill.total, bill.date, bill.month, bill.year, bill.status, bill.type_, bill.product_name, bill.username])
       print(t)
-  
   @staticmethod
   def show_my_filtered_bills(db, filtered_bills):
     if len(filtered_bills) != 0:
-      print("okay")
+      
       t = PrettyTable(['ID','Total','Date','Month','Year','Status','Type','Product Name','Username'])
       for bill in filtered_bills:
         t.add_row([bill.ID, bill.total, bill.date, bill.month, bill.year, bill.status, bill.type_, bill.product_name, bill.username])
--- main.py
+++ main.py
@@ -1,117 +1,120 @@
 from database import Database
 from bills import *
-from identity import Account,Accounts,Login
+from identity import *
 from report import Reports
 from Graph import *
 from report import *
+from clear_screen import clear
+from time import sleep 
 #Grupi 4 esht jet <3
-
+import colorama
+from colorama import Fore
 def startSession(db,User):
   while(True):
-  #ManageBills.createBill(db)
-  # menuja kryesore 
-    #manage bills
-    #manage Account
-    #reports
-    #FindMonthlyTotal.findTotal(self,MathiasD)
     print("Press 1 to add bills.")
     print("Press 2 to filer bills.")
     print("Press 3 to delete bills.")
-    print("Press 4 to show all your bills. ")
-    print("Press 5 to acces report pannel.")
-    print("Press 6 to acces graph.")
-    print("Press 7 to show all your unpaid bills.")
-    print("Press 8 to show some website to gather information about paying your bills.")
-    print("Press 9 to change your password.")
-    print("Press 0 to log out.\n")
-
-    choice = input(">")
-
-    if(choice=='1'):
+    print("Press 4 to access bill panel. ")
+    print("Press 5 to access report pannel.")
+    print("Press 6 to access graph.")
+    print("Press 7 to show some websites to gather further information about paying your bills.")
+    print("Press 8 to change your password.")
+    print("Press 9 to delete your account.")
+    print("Press 0 to log out.")
+    choice = int(input(">"))
+    if(choice==1):
       ManageBills.createBill(db,User)
-
-    elif (choice == '2'):
+    elif (choice == 2):
       ManageBills.filterBill(db,User)
       
-    elif (choice == '3'):
-      ManageBills.deleteBill(db,User)
-      
-    elif (choice == '4'):
-      ManageBills.showmybills(db,User)
-    elif (choice == '5'):
-      print("\n\nWelcome to the report pannel!\n")
-      print("Press 1 to get a daily report, for all the bills paid in the same day!")
-      print("Press 2 to get a mothly report, for all the bills paid within a same month!")
-      print("Press 3 to get a yearly report, for all the bills paid within a same year!\n\n")
-      usr_input = input("Input: ")
-      if usr_input == '1':
-        date = input("Please give us the day you'd like a report of divided with / :")
-        print("The daily report of the", date, "is", Reports.showDailyReport(db,User,date))
-      elif usr_input == '2':
-        date = input("Give the month and year divded with / :")
-        print("The monthly report of the", date, "is", Reports.showMonthlyReport(db, User, date))
-      elif usr_input == '3':
-        date = input("Give the year: ")
-        print("The monthly report of the", date, "is", Reports.showYearlyReport(db, User, date))
+    elif (choice == 3):
+      ManageBills.deleteBill(db,User)     
+    elif (choice == 4):
+      print("Press 1 to show all your bills, press 2 to show all your unpaid bill or press 3 to go back to the main menu. ")
+      input12=int(input(">"))
+      if input12==1:
+        ManageBills.showmybills(db,User)
+      elif input12==2:
+        ManageBills.unpaid_bills(db,User)
       else:
         pass
-    elif (choice == '6'):
+    elif (choice == 5):
+      print("\nWelcome to the report pannel!")
+      print("Press 1 to get a daily report, for all the bills paid in the same day!")
+      print("Press 2 to get a mothly report, for all the bills paid within a same month!")
+      print("Press 3 to get a yearly report, for all the bills paid within a same year!\n")
+      usr_input = int(input("Input: "))
+      if usr_input == 1:
+        print("Please give us the day you'd like a report of divided with / :")
+        date = input(">")
+        print("\n\nThe daily report of the", date, "is", Reports.showDailyReport(db,User,date))
+      elif usr_input == 2:
+        print("Give the month and year divded with / :")
+        date = input(">")
+        print("\n\nThe monthly report of the", date, "is", Reports.showMonthlyReport(db, User, date))
+      elif usr_input == 3:
+        print("Give the year: ")
+        date = input(">")
+        print("\n\nThe monthly report of the", date, "is", Reports.showYearlyReport(db, User, date))
+      else:
+        pass
+    elif (choice == 6):
       CreateGraph(db,User)
-    elif (choice == '7'):
-      ManageBills.unpaid_bills(db,User)
-    elif(choice=='8'):
+      print("\n")
+      input("Press Enter to continue...")
+    elif(choice==7):
       paying()
-    elif(choice == '9'):
-      Accounts.changePassword(db)
-    elif (choice == '0'):
-      User == None
-      break
+    elif(choice == 8):
+      Accounts.changePassword(db,User)
+    elif(choice==9):
+      Accounts.deleteAcc(db,User)
+      return
+    elif (choice == 0):
+      return
     else:
       continue
-
 def main():
+  print(Fore.BLUE)
   print("          (         )            (               ")
   print(" (        )\ )   ( /(            )\ )         )  ")
   print(" )\ )    (()/(   )\())      (   (()/(      ( /(  ")
   print("(()/(     /(_)) ((_)\       )\   /(_))     )\()) ")
   print(" /(_))_  (_))     ((_)   _ ((_) (_))      ((_)\  ")
-  print("(_)) __| | _ \   / _ \  | | | | | _ \    | | (_) ")
+  print(Fore.RESET,end='')
+  print(Fore.RED,end='')
+  print("   / __| | _ \   / _ \  | | | | | _ \    | |  | ")
   print("  | (_ | |   /  | (_) | | |_| | |  _/    |_  _|  ")
-  print("   \___| |_|_\   \___/   \___/  |_|        |_|   ")
-
+  print("   \___| |_|_\   \___/   \___/  |_|        |_|   \n\n")
+  print(Fore.RESET)
   db = Database(dbname="database")
-  # create tables here 
   db.createtableifnotexists("All Bills", AllBills,AllBills.fromstring)
-
-
   db.createtableifnotexists("Accounts",Account,Account.fromstring)
   user = None
-  
   while(True):
-    # check if there is any account 
-    all_accounts=db.getObjectsFrom("Accounts", lambda a:True)
-    if len(all_accounts) == 0:
-      print("Register the account: ")
-      Accounts.createAcc(db)
-    print("Press 1 for Login\nPress 2 to register\nPress 3 for exiting")
-
-    a = input(">")
-    if a=='1':
-      user=Login.CheckCredentials(db)
-      if user==None:
-        continue  
+    now=True
+    while now==True:
+      try:
+        print("Press 1 for Login\nPress 2 to register\nPress 3 for exiting")
+        a=int(input())
+        if a==1 or a==2 or a==3:
+          now==False
+      except Exception: 
+          print("Please don't be a jerk.")
+          continue
+      
+      if a==1:
+        user=Login.CheckCredentials(db)
+        if user==None:
+          continue
+          clear_screen.clear()  
+        else:
+          startSession(db,user)
+          clear_screen.clear()  
+      elif a==2:
+        Accounts.createAcc(db)
+      elif a==3:
+        return
       else:
-         startSession(db,user)
-    elif a=='2':
-      Accounts.createAcc(db)
-    elif a=='3':
-      return
-    
-    elif a=='4':
-      pass
-    else:
-      print("Not a valid choice!")
-      continue
-
+        continue
 if __name__ == '__main__':
   main()
